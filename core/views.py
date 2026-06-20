b9c1ca7e6912afbde8b25a95d12829e3d75f5dc8
kushalpaudelDev <kushalpaudel589@gmail.com>
2026-06-20 14:37:07 +0545

    update in views

--- core/views.py
+++ core/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from pets.models import Pet
+from appointments.models import Appointment
 
 def home(request):
     return render(request, "home.html")
+
+
+def dashboard(request):
+    total_pets = Pet.objects.count()
+    total_appointments = Appointment.objects.count()
+    pending = Appointment.objects.filter(status="pending").count()
+    completed = Appointment.objects.filter(status="completed").count()
+
+    context = {
+        "total_pets": total_pets,
+        "total_appointments": total_appointments,
+        "pending": pending,
+        "completed": completed,
+    }
+
+    return render(request, "dashboard.html", context)
