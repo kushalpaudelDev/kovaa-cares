f68b7ee2c4a09d1879314de856dd188b29f4af31
kushalpaudelDev <kushalpaudel589@gmail.com>
2026-06-20 15:20:19 +0545

    updates in admin for diplay and filter

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Appointment
 
-# Register your models here.
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ("pet", "status", "date")
+    list_filter = ("status", "date")
