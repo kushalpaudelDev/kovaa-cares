0ee7b9dc28c708e88d31a2b75e102600472c045e
kushalpaudelDev <kushalpaudel589@gmail.com>
2026-06-21 09:24:29 +0545

    urls updates /pets

--- pets/urls.py
+++ pets/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.pet_list, name='pet_list'),
+    path('create/', views.pet_create, name='pet_create'),
+    path('update/<int:pk>/', views.pet_update, name='pet_update'),
+    path('delete/<int:pk>/', views.pet_delete, name='pet_delete'),
 ]
