258b807504cfc66a7c3aac66342aeed15c1851e2
kushalpaudelDev <kushalpaudel589@gmail.com>
2026-06-22 18:22:38 +0545

    followup

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Service
 
-# Register your models here.
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'duration')
+    search_fields = ('name', 'description')
+
+
+admin.site.register(Service, ServiceAdmin)
