7b1076a5d101ad345529ab99e2e90578ea8bfbcf
kushalpaudelDev <kushalpaudel589@gmail.com>
2026-06-22 18:18:41 +0545

    up

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Notification
 
-# Register your models here.
+
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'is_read', 'created_at')
+    list_filter = ('is_read',)
+    search_fields = ('title', 'message', 'user__username')
+
+
+admin.site.register(Notification, NotificationAdmin)
