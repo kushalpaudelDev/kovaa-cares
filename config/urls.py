959b200eb7f2426ec5843339418a72237862d25b
kushalpaudelDev <kushalpaudel589@gmail.com>
2026-06-22 09:11:46 +0545

    update urls

--- config/urls.py
+++ config/urls.py
@@ -1,17 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
 
 admin.site.site_header = "Kovaa Cares Admin"
 admin.site.site_title = "Kovaa Cares Portal"
 admin.site.index_title = "Welcome to Kovaa Cares Dashboard"
 
-# simple dashboard (no model import)
-def dashboard(request):
-    return HttpResponse("Kovaa Cares Dashboard Working")
-
 urlpatterns = [
-    path('admin/dashboard/', dashboard),
     path('admin/', admin.site.urls),
 
     path('', include('core.urls')),
