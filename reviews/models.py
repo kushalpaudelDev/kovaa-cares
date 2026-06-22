47a934cda86700692e221e6dd0f5cadbe769a0aa
kushalpaudelDev <kushalpaudel589@gmail.com>
2026-06-22 09:46:24 +0545

    updates in models

--- reviews/models.py
+++ reviews/models.py
@@ -4,5 +4,9 @@ from django.contrib.auth.models import User
 
 class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-  
+    def __str__(self):
+        return f"{self.user.username} - {self.rating}" 
