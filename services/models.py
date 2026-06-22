369ef0dc03ead67c6fbf588ace52349eb008eb7c
kushalpaudelDev <kushalpaudel589@gmail.com>
2026-06-22 09:14:43 +0545

    intalizing model

--- services/models.py
+++ services/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    duration = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
