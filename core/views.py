ca52f6cd270c90681f05e58f00c1ae71cbb1a944
kushalpaudelDev <kushalpaudel589@gmail.com>
2026-06-22 09:10:57 +0545

    updates

--- core/views.py
+++ core/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from pets.models import Pet
 from appointments.models import Appointment
-from django.contrib.auth.decorators import login_required
+
 
 def home(request):
     return render(request, "home.html")
 
+
 @login_required
 def dashboard(request):
+
     total_pets = Pet.objects.count()
     total_appointments = Appointment.objects.count()
+
     pending = Appointment.objects.filter(status="pending").count()
     completed = Appointment.objects.filter(status="completed").count()
 
--- pets/models.py
+++ pets/models.py
@@ -1,8 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Pet(models.Model):
+    GENDER_CHOICES = [
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+    ]
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
     name = models.CharField(max_length=100)
-    type = models.CharField(max_length=50)
+    species = models.CharField(max_length=50)   # dog, cat
+    breed = models.CharField(max_length=100, blank=True)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+    vaccinated = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
