d3db9e4d955b34c02555c7d123d9d10c0dd5b9a7
kushalpaudelDev <kushalpaudel589@gmail.com>
2026-06-22 18:22:06 +0545

    up

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'rating', 'created_at')
+    list_filter = ('rating',)
+    search_fields = ('user__username', 'comment')
+
+
+admin.site.register(Review, ReviewAdmin)
