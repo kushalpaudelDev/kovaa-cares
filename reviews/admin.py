083715919a7c67d7bdfbae9717984736ec73bd35
kushalpaudelDev <kushalpaudelDev@users.noreply.github.com>
2026-06-26 12:25:05 +0545

    upates in admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -4,8 +4,13 @@ from .models import Review
 
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('user', 'rating', 'created_at')
+    list_display_links = ('user',)
     list_filter = ('rating',)
     search_fields = ('user__username', 'comment')
+    date_hierarchy = 'created_at'
+    autocomplete_fields = ('user',)
+    list_per_page = 25
+    ordering = ('-created_at',)
 
 
 admin.site.register(Review, ReviewAdmin)
